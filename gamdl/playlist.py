90c64564140660ea88014323b9b1a175103454c2
Sam Büth <samuel1bueth@gmail.com>
2024-06-07 12:32:04 +0200

    Refactor playlist path handling and add support for saving playlists to a separate directory

--- gamdl/playlist.py
+++ gamdl/playlist.py
@@ -17,8 +17,10 @@ class Playlist:
         self.output_path = output_path.__str__()
 
     def add_path(self, path):
-        self.path_list.append(path.__str__().replace(self.output_path + "/", ""))
+        self.path_list.append(path.__str__().replace(self.output_path + "/", "../Apple Music/"))
 
     def save_to_file(self):
-        with open(os.path.join(self.output_path, self.name + ".m3u"), 'w') as f:
+        playlist_path = os.path.join(self.output_path, "..", "Playlists")
+        os.makedirs(playlist_path, exist_ok=True)
+        with open(os.path.join(playlist_path, self.name + ".m3u"), 'w') as f:
             f.write('\n'.join(self.path_list) + "\n")
